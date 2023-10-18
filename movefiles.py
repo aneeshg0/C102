aba0a91243160e6e942f0eca0630c946640c01d3
aneeshg0 <114878021+aneeshg0@users.noreply.github.com>
2023-10-18 20:29:30 +0100

    Add files via upload

---
+++ movefiles.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+from_dir = "images"
+to_dir = "images"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+for file_name in list_of_files:
+    name, extension = os.path.splitext(file_name)
